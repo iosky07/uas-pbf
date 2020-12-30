1fea63a4dae79784804875cfe904769d8207e696
noobiesdev <182410101083@students.unej.ac.id>
2020-12-30 23:32:50 +0700

    try push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,7 +88,7 @@
             <div class="col-lg-6 col-xl-6">
                 <div class="banner_text">
                     <div class="banner_text_iner">
-                        <h5>Every child yearns to learn</h5>
+                        <h5>Every child yearns to play</h5>
                         <h1>Making Your Childs
                             World Better</h1>
                         <p>Replenish seasons may male hath fruit beast were seas saw you arrie said man beast whales
