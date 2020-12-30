efa6f1dd2c2c70f8c8f86efbd7b663ea334a6a36
iosky07 <yoskitanjung07@gmail.com>
2020-12-31 01:38:22 +0700

    add krlove + model

---
+++ app/Article.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property integer $id_user
+ * @property string $title
+ * @property string $content
+ * @property string $created_at
+ * @property string $updated_at
+ * @property User $user
+ * @property Comment[] $comments
+ */
+class Article extends Model
+{
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['id_user', 'title', 'content', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'id_user');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments()
+    {
+        return $this->hasMany('App\Comment', 'id_article');
+    }
+}
