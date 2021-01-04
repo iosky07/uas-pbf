946d08c8e4ee3d971ae010d88e8e4d377ade29cb
iosky07 <yoskitanjung07@gmail.com>
2021-01-04 22:25:54 +0700

    update

--- app/User.php
+++ app/User.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -20,11 +22,12 @@ use Illuminate\Database\Eloquent\Model;
  * @property Comment[] $comments
  * @property Suggestion[] $suggestions
  */
-class User extends Model
+class User extends Model implements Authenticatable
 {
+    use AuthenticableTrait;
     /**
      * The "type" of the auto-incrementing ID.
-     * 
+     *
      * @var string
      */
     protected $keyType = 'integer';
