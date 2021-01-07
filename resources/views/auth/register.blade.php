a1092fa12f6ecc448a23a6979f9ec2f6b59ee542
iosky07 <yoskitanjung07@gmail.com>
2021-01-07 05:13:03 +0000

    completing part 2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,7 +24,7 @@
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                     <div class="login-brand">
-                        <img src="{{asset('stisla/assets/img/stisla-fill.svg')}}" alt="logo" width="100" class="shadow-light rounded-circle">
+                        <img src="{{asset('frontend/img/favicon.png')}}" alt="logo" width="70" class="shadow-light">
                     </div>
 
                     <div class="card card-primary">
@@ -97,7 +97,7 @@
                         Udah Punya Akun? <a href="{{ route('login') }}">Login Disini</a>
                     </div>
                     <div class="simple-footer">
-                        Copyright &copy; Stisla 2018
+                        Bears Team 2 - Kelompok 7 2021
                     </div>
                 </div>
             </div>
